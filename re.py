dc5342fb6845778804d29d63fd1437b2e825b646
rajataneja101 <tanejarajat101@gmail.com>
2017-02-17 00:00:36 +0530

    regular expression

---
+++ re.py
@@ -0,0 +1,8 @@
+import re
+
+#print(re.split(r'(s*)','here are some words'))
+
+#print(re.split(r'[a-fA-F]','djsdssAGJJHSIW'))
+print(re.match(r'[^\d]\w*\@\w+[.]+\w+','rajat101@gmai.l.com').group())
+print(re.findall(r'[^\d]\w+\@\w+\.+\w+','123rajat101@gmail.com'))
+print(re.match(r'[^\d]\w*\@\S+','rajat101@gmai.l.com').group())
